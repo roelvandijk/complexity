a7f5ed8e30839e4735110c36c36c5a1361bf7523
Roel van Dijk <vandijk.roel@gmail.com>
2009-04-07 00:32:19 +0200

    Added Misc module
    Ignore-this: d66837cf8784521d49e36b06586f2dcd
    
    darcs-hash:20090406223219-b004c-443c7c160e428a611707ff70e8d821dcf3d81b72.gz

---
+++ Test/Complexity/Misc.hs
@@ -0,0 +1,27 @@
+module Test.Complexity.Misc ( (>>=|)
+                            , (>>|)
+                            , strictReplicateM_
+                            , diff
+                            , picoToMilli
+                            ) where
+
+import Control.Parallel.Strategies (NFData, rnf, ($|))
+
+-- |Very strict monadic bind
+(>>=|) :: (Monad m, NFData a) => m a -> (a -> m b) -> m b
+m >>=| f = m >>= f $| rnf
+
+(>>|) :: (Monad m, NFData a) => m a -> m b -> m b
+m1 >>| m2 = m1 >>=| const m2
+
+strictReplicateM_ :: NFData a => Int -> IO a  -> IO ()
+strictReplicateM_ n a = go n
+    where go 0 = return ()
+          go n = a >>| go (n - 1)
+
+-- |Difference
+diff :: Num a => a -> a -> a
+diff x y = abs $ x - y
+
+picoToMilli :: Integer -> Double
+picoToMilli p = (fromInteger p) / 1e9
